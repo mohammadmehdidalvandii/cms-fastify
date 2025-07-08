7cec227c59d9f80f7d37de47848f7a6f8a58cbf9
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-07-08 10:42:03 +0330

    create user.services v-1

---
+++ src/services/users.services.js
@@ -0,0 +1,28 @@
+const UserModel = require('../models/Users')
+
+async function register(name , email , phone , password , role) {
+    const user = await UserModel.create({name , email , phone , password , role});
+    return user
+}
+
+async function getUserByEmail(email){
+    const user = await UserModel.findOne({email});
+    return user
+}
+
+async function getUserByID (id){
+    const user = await UserModel.findOne({_id:id})
+    return user
+}
+
+async function login(email , password) {
+    const user = await UserModel.findOneAndUpdate(email , password);
+    return user
+}
+
+module.exports = {
+    register,
+    getUserByEmail,
+    getUserByID,
+    login,
+}
